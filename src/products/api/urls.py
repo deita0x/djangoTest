deda6561c8fb08d9ed6d2f196b40d38b50b63cf3
José De Ita <josdeita@gmail.com>
2018-12-18 14:29:53 -0600

    initial commit

---
+++ src/products/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ProductListView, ProductDetailView
+
+
+urlpatterns = [
+    path('', ProductListView.as_view()),
+    path('<pk>', ProductDetailView.as_view()),
+]
---
+++ src/products/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from products.models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
